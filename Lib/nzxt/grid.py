630ec45a81b87fe689761133ce6207fd40bd9b7c
Matthias Riegler <matthias@xvzf.tech>
2016-12-12 21:20:36 +0100

    More documentation, some fixes, more coming soon

--- Lib/nzxt/grid.py
+++ Lib/nzxt/grid.py
@@ -18,23 +18,40 @@ class Grid(object):
     ID_GET_RPM      = 138
     ID_SET_VOLTAGE  = 68
     ID_GET_VOLTAGE  = 132
+    ID_GET_CURRENT  = 133
 
     # Maximum voltage, NZXT uses 12.4 in their software, however if I send 12.4v to the fan controller,
     # it does response with an OK, but it doesn't change the voltage. This problem appears on windows
     # side too, if I set the fan speed to 100% in the CAM Software, there is no change in fan speed.
-    max_voltage     = 12.0
+    max_voltage         = 12.0
+    max_response_wait   = 6
 
     def __init__(self, port):
         super(Grid, self).__init__()
         self.port = port
-        self.connection = serial.Serial(port,baudrate=4800)
+        self.connection = serial.Serial(port,baudrate=4800) # Maybethey think that no error correction 
+                                                            # is needed 
 
 
     def transmit(self, buffer, bytes_to_read):
-        # TODO ERROR HANDLING
         self.connection.write(buffer)
-        time.sleep(0.25)
+
+        # Do not wait longer than 0.5 seconds for a response
+        totalwait = 0
+        while self.connection.inWaiting() != bytes_to_read and totalwait < self.max_response_wait:
+            time.sleep(0.1)
+            totalwait = totalwait + 1
+
+        # If there are more bytes in the fifo than expected, there might be an error in the transmission
+        # Drop everything so the next transmission works again.
+        if self.connection.inWaiting() > bytes_to_read:
+            self.connection.read(self.connection.inWaiting())
+            return []
+
         tmp = self.connection.read(self.connection.inWaiting())
+
+        # Converting the character array to an int array makes it easier to implement further operations without
+        # converting the array in each individually
         returnbuffer = []
         for i in tmp:
             returnbuffer.append(ord(i))
@@ -51,6 +68,7 @@ class Grid(object):
             return -1
 
 
+    # The rpm is transmitted in two bytes, which are combined in either uint16_t or int16_t (does not matter)
     def get_rpm(self, index):
         if index not in range(1,7):
             return -1
@@ -65,6 +83,8 @@ class Grid(object):
         return (int(response[3]) << 8) + int(response[4])
 
 
+    # The voltage is transmitted in a 2 byte block. first byte covers the decimal value and the 2nd byte 
+    # responses to the 1st and 2nd digit.
     def set_voltage(self, index , voltage):
         # I don't think the fan controller can handle precision up to 0.01v, so I dropped the support
         vsteps = int(voltage*10) # Preparations for linux kernel module integration
@@ -77,14 +97,18 @@ class Grid(object):
         return self.checkresponse(response)
 
 
+    # Should be self explaining
     def set_percentage(self, index, p):
         
         if p > 100:
             return -1
        
-        return self.set_voltage(index, (p/100.0) * max_voltage)
+        return self.set_voltage(index, (float(p)/100.0) * self.max_voltage)
 
 
+    # The requested answer is provided in the same format as the voltage is set. I don't think the internal
+    # measurement is precise - I measured 12.13v on one fan out put using my multimeter (not calibrated!) -
+    # get_voltage() returns 11.4, which a 0.5v difference.
     def get_voltage(self, index):
 
         sendbuffer = [self.ID_GET_VOLTAGE, index]
@@ -100,6 +124,9 @@ class Grid(object):
         return float(tmp_voltage)/10.0
 
 
+    # Get the current so we can calculate the power afterwards. Usefull to check if a fan is connected
+    # Maybe not that precise - I still have to measure the precision.
     def get_current(self, index):
+        # return System.Math.Round((double)bytes[3] + (double)(bytes[4] / 16) / 10.0 + (double)(bytes[4] % 16) / 100.0, 2);
         #TODO
         pass
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 CAM4LINUX is a python library and software suite for nzxt devices.
 It is based on reverse engineering the CAM software provided by nzxt.
 
+
+# SUPPORTED DEVICES
+- NZXT Grid
+
 # LICENSE
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
