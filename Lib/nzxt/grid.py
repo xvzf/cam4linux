da58cb8d23e6f02f8a4983099e1fef47b4e46c48
Matthias Riegler <s8marieg@stud.uni-saarland.de>
2016-12-25 22:53:31 +0100

    Thrading support, moving towards service

--- Lib/nzxt/grid.py
+++ Lib/nzxt/grid.py
@@ -11,14 +11,15 @@ GNU General Public License for more details.
 
 import serial
 import time
+from thread import start_new_thread
 
 class Grid(object):
 
     # IDs based on reverse engineering the "protocol"
-    ID_GET_RPM      = 138
-    ID_SET_VOLTAGE  = 68
-    ID_GET_VOLTAGE  = 132
-    ID_GET_CURRENT  = 133
+    ID_GET_RPM          = 138
+    ID_SET_VOLTAGE      = 68
+    ID_GET_VOLTAGE      = 132
+    ID_GET_CURRENT      = 133
 
     # Maximum voltage, NZXT uses 12.4 in their software, however if I send 12.4v to the fan controller,
     # it does response with an OK, but it doesn't change the voltage. This problem appears on windows
@@ -26,14 +27,25 @@ class Grid(object):
     max_voltage         = 12.0
     max_response_wait   = 6
 
+    # Update interval in seconds
+    poll_rate           = 2
+    everythingok        = False
+
+    # Buffer Array for the update thread
+    _rpms               = [0,0,0,0,0,0] 
+    _powers             = [0.0,0.0,0.0,0.0,0.0,0.0]
+
     def __init__(self, port):
         super(Grid, self).__init__()
         self.port = port
         self.connection = serial.Serial(port,baudrate=4800) # Maybethey think that no error correction 
-                                                            # is needed 
+                                                            # is needed when they use low baud rates
+        
+        # Lets start an update routine so we don't have to deal with the slow response times!
+        self.everythingok = self.connection.isOpen()
+        start_new_thread(self._updatethread, ()) # Not the prettiest syntax, however it works
 
-
-    def transmit(self, buffer, bytes_to_read):
+    def _transmit(self, buffer, bytes_to_read):
         self.connection.write(buffer)
 
         # Do not wait longer than 0.5 seconds for a response
@@ -58,7 +70,7 @@ class Grid(object):
         return returnbuffer
 
 
-    def checkresponse(self, response):
+    def _checkresponse(self, response):
         if len(response) != 1:
             return -1
 
@@ -68,13 +80,17 @@ class Grid(object):
             return -1
 
 
-    # The rpm is transmitted in two bytes, which are combined in either uint16_t or int16_t (does not matter)
-    def get_rpm(self, index):
+    # The rpm is _transmitted in two bytes, which are combined in either uint16_t or int16_t (does not matter)
+    def _get_rpm(self, index):
+
+        # Indexes starting at 1
+        index = index + 1
+
         if index not in range(1,7):
             return -1
 
         sendbuffer = [self.ID_GET_RPM, index]
-        response = self.transmit(sendbuffer, 5)
+        response = self._transmit(sendbuffer, 5)
         
         # Check if error occured
         if len(response) != 5:
@@ -83,37 +99,34 @@ class Grid(object):
         return (int(response[3]) << 8) + int(response[4])
 
 
-    # The voltage is transmitted in a 2 byte block. first byte covers the decimal value and the 2nd byte 
+    # The voltage is _transmitted in a 2 byte block. first byte covers the decimal value and the 2nd byte 
     # responses to the 1st and 2nd digit.
-    def set_voltage(self, index , voltage):
+    def _set_voltage(self, index , voltage):
+
+        # Indexes starting at 1
+        index = index + 1
+
         # I don't think the fan controller can handle precision up to 0.01v, so I dropped the support
         vsteps = int(voltage*10) # Preparations for linux kernel module integration
                                  # no floating point in kernelspace so we use 0.1v steps!
         
         sendbuffer = [self.ID_SET_VOLTAGE, index ,192, 0, 0, (vsteps/10), ((vsteps % 10) << 4)]
         
-        response = self.transmit(sendbuffer, 1)
+        response = self._transmit(sendbuffer, 1)
 
-        return self.checkresponse(response)
-
-
-    # Should be self explaining
-    def set_percentage(self, index, p):
-        
-        if p > 100:
-            return -1
-       
-        return self.set_voltage(index, (float(p)/100.0) * self.max_voltage)
-
+        return self._checkresponse(response)
 
     # The requested answer is provided in the same format as the voltage is set. I don't think the internal
     # measurement is precise - I measured 12.13v on one fan out put using my multimeter (not calibrated!) -
     # get_voltage() returns 11.4, which a 0.5v difference.
-    def get_voltage(self, index):
+    def _get_voltage(self, index):
+
+        # Indexes starting at 1
+        index = index + 1
 
         sendbuffer = [self.ID_GET_VOLTAGE, index]
 
-        response = self.transmit(sendbuffer, 5)
+        response = self._transmit(sendbuffer, 5)
 
         if len(response) != 5:
             return -1.0
@@ -126,11 +139,14 @@ class Grid(object):
 
     # Get the current so we can calculate the power afterwards. Usefull to check if a fan is connected
     # Maybe not that precise - I still have to measure the precision.
-    def get_current(self, index):
+    def _get_current(self, index):
         
+        # Indexes starting at 1
+        index = index + 1
+
         sendbuffer = [self.ID_GET_CURRENT, index]
 
-        response = self.transmit(sendbuffer, 5)
+        response = self._transmit(sendbuffer, 5)
 
         if len(response)  != 5:
             return -1.0
@@ -141,3 +157,38 @@ class Grid(object):
 
 
         return float(tmp_current)/1000.0
+
+    # This functions updates all values at a given update intervall
+    def _update(self):
+        # The device supports 6 fans attached 
+        for i in range(0,6):
+            self._rpms[i] = self._get_rpm(i)                                 # Update_rpm
+            self._powers[i] = self._get_voltage(i) * self._get_current(i)    # Update power usage
+
+    # Should be self explaining
+    def _updatethread(self):
+        while self.everythingok:
+            self._update()
+            time.sleep(self.poll_rate)
+
+    # Should be self explaining as well
+    def set_percentage(self, index, p):
+        
+        if p > 100:
+            return -1
+       
+        return self._set_voltage(index, (float(p)/100.0) * self.max_voltage)
+
+    # Get RPM
+    def get_rpm(self,index):
+        if index in range(0,6):
+            return self._rpms[index]
+        else:
+            return -1
+
+    # Get Power
+    def get_power(self,index):
+        if index in range(0,6):
+            return self._powers[index]
+        else:
+            return -1.0
